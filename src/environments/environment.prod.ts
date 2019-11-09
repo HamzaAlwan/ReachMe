c295ff74c29aa0d1cb49c1b97932130247e93701
HamzaAlwan <ihamzaalwan@gmail.com>
2019-11-09 15:41:07 +0200

    updated api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // api: "https://mychatapp-project.herokuapp.com/api/chatapp"
-  api: "http://localhost:5500/api/chatapp",
+  api: "https://mychatapp-project.herokuapp.com/api/chatapp",
   socket_io: "https://mychatapp-project.herokuapp.com"
 };
